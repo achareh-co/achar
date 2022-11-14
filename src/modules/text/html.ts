fa9ff0d47a5ba21ce1b5d5f323268b554d5eaf14
saleh <ms.fadaei@gmail.com>
2022-11-14 19:39:38 +0330

    test: add `html` tests

--- src/modules/text/html.ts
+++ src/modules/text/html.ts
@@ -11,3 +11,32 @@ export default function html(template: Template, ...substitution: unknown[]) {
     .trim()
     .replace(/\s\s+/g, ' ');
 }
+
+if (import.meta.vitest) {
+  const { it, expect } = import.meta.vitest;
+
+  it('process a simple paragraph', () => {
+    const entry = '<p>this is simple html paragraph with some texts</p>';
+    const result = entry;
+
+    expect(html`${entry}`).to.be.equal(result);
+  });
+
+  it('process a simple paragraph with attributes', () => {
+    const entry = '<p id="text" class="normal-text">this is simple html paragraph</p>';
+    const result = entry;
+
+    expect(html`${entry}`).to.be.equal(result);
+  });
+
+  it('process a simple paragraph with attributes and extra spaces', () => {
+    const entry = `
+      <p id="text"  class="normal-text">
+        this is simple html paragraph
+      </p>
+    `;
+    const result = '<p id="text" class="normal-text"> this is simple html paragraph </p>';
+
+    expect(html`${entry}`).to.be.equal(result);
+  });
+}
