2d9671d67bbcc8514a6c7cd6a89af836f9dbe3d2
Mohammad Saleh Fadaei <ms.fadaei1997@gmail.com>
2024-04-29 17:48:11 +0330

    test: add tests

--- src/modules/text/thousand-separator.ts
+++ src/modules/text/thousand-separator.ts
@@ -13,10 +13,14 @@ if (import.meta.vitest) {
   const { it, expect } = import.meta.vitest;
 
   it('separate thousands with comma in a text', () => {
+    expect(thousandSeparator('')).to.be.equal('');
+    expect(thousandSeparator(0)).to.be.equal('0');
     expect(thousandSeparator('1')).to.be.equal('1');
+    expect(thousandSeparator(1)).to.be.equal('1');
     expect(thousandSeparator('123')).to.be.equal('123');
     expect(thousandSeparator('1234')).to.be.equal('1,234');
     expect(thousandSeparator('123456')).to.be.equal('123,456');
+    expect(thousandSeparator(123456)).to.be.equal('123,456');
     expect(thousandSeparator('1234567890')).to.be.equal('1,234,567,890');
     expect(thousandSeparator('this a text with 1 and 12345')).to.be.equal(
       'this a text with 1 and 12,345',
